36873a2eb58b50d0e467800cd6e4abe3fbaaf5fb
rajyavardhanbithale <bithale03@gmail.com>
2024-02-29 20:35:20 +0530

    added function to exit and maxi

--- app/component/desktop/TopBar.jsx
+++ app/component/desktop/TopBar.jsx
@@ -1,8 +1,33 @@
-
+'use client'
 import { IonIcon } from '@ionic/react'
 import './desktop.css'
-import { cloudDoneOutline, contract, earthOutline, fingerPrintOutline, powerOutline, sunnyOutline } from 'ionicons/icons'
+import { cloudDoneOutline, contract, earthOutline, fingerPrintOutline, power, powerOutline, sunnyOutline } from 'ionicons/icons'
 export default function TopBar() {
+
+    function handleActionButton(action) {
+        switch (action) {
+            case 'power':
+                window.open("about:blank", "_self");
+                window.close();
+                break;
+
+            case 'maxi':
+                const element = document.documentElement;
+                if (element.requestFullscreen) {
+                  element.requestFullscreen();
+                } else if (element.mozRequestFullScreen) {
+                  element.mozRequestFullScreen();
+                } else if (element.webkitRequestFullscreen) {
+                  element.webkitRequestFullscreen();
+                } else if (element.msRequestFullscreen) {
+                  element.msRequestFullscreen();
+                }
+
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <div className="h-9 glass-top flex justify-between">
@@ -11,10 +36,18 @@ export default function TopBar() {
                 </div>
 
                 <div className="flex gap-4 px-4 py-1.5 text-2xl">
+
                     <IonIcon icon={cloudDoneOutline}></IonIcon>
                     <IonIcon icon={earthOutline}></IonIcon>
-                    <IonIcon icon={contract}></IonIcon>
-                    <IonIcon icon={powerOutline}></IonIcon>
+                    <div onClick={() => handleActionButton('maxi')} >
+                        <IonIcon icon={contract}></IonIcon>
+
+                    </div>
+
+                    <div onClick={() => handleActionButton('power')} >
+                        <IonIcon icon={powerOutline}></IonIcon>
+
+                    </div>
                 </div>
             </div>
 
